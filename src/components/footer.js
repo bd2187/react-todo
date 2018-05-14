66ffe9404b4f0372c7db85fa00703b99a2f5738b
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-13 19:13:22 -0700

    Establish constructor in Footer component. Create clearCompleted function which will invoke this.props.clearCompleted(). Add style attribute to filter buttons. Depending on the filter state, change the background color.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.clearCompleted = this.clearCompleted.bind(this);
+  }
+
   changeFilter(filter) {
     this.props.changeFilter(filter);
   }
 
+  clearCompleted() {
+    this.props.clearCompleted();
+  }
+
   render() {
     var numberOfTodos = this.props.numberOfTodos;
+    var filter = this.props.filter;
 
     return (
       <div>
@@ -15,22 +25,27 @@ class Footer extends Component {
         <button
           id="all_todos_btn"
           onClick={this.changeFilter.bind(this, "none")}
+          style={{ backgroundColor: filter === "none" ? "red" : null }}
         >
           All
         </button>
         <button
           id="active_todos_btn"
           onClick={this.changeFilter.bind(this, "active")}
+          style={{ backgroundColor: filter === "active" ? "red" : null }}
         >
           Active
         </button>
         <button
           id="complete_todos_btn"
           onClick={this.changeFilter.bind(this, "completed")}
+          style={{ backgroundColor: filter === "completed" ? "red" : null }}
         >
           Complete
         </button>
-        <button id="clear_completed_btn">Clear Completed</button>
+        <button id="clear_completed_btn" onClick={this.clearCompleted}>
+          Clear Completed
+        </button>
       </div>
     );
   }
